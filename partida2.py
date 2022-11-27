981490dbee35b591fec8ea5eb711435cb6f52fb1
chankey_uwu <carlos.kuhn@sansano.usm.cl>
2022-11-27 02:47:28 -0300

    esto en verdad es la cola

--- partida2.py
+++ partida2.py
@@ -3,12 +3,19 @@ from datetime import datetime
 from threading import *
 from registros import *
 
-class Partida():
+class cola_partida():
     def __init__(self, game_id, queue_capacity):
         self.game_id = game_id
         self.queue_capacity = queue_capacity
-        self.sem_enqueue = Semaphore(self.queue_capacity)
-        self.queue = list()
+        self.queue_quantity = 0
+        self.lock = Lock()
+        self.fullQueue = False
 
-    def enqueue(self, jugador):
-        self.sem_enqueue.acquire()
+    def isFull(self):
+        return self.fullQueue
+    
+    def enqueue(self, player):
+        self.lock.acquire()
+        self.queue_quantity += 1
+        if self.queue_capacity == self.queue_quantity:
+            self.fullQueue = True
