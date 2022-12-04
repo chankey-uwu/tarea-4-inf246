c5cee52c99c11e835db37eac7f97ce2c26ef5c74
chankey-uwu <carlos.kuhn@usm.cl>
2022-12-04 20:48:42 -0300

    cola

--- lobby.py
+++ lobby.py
@@ -9,18 +9,27 @@ class Lobby():
     def __init__(self, partidas):
         self.partidas = partidas
         self.locks = [Lock(),Lock(),Lock(),Lock()]
+        self.queues = [[],[],[],[]]
     
     # jugador: objeto 'jugador'
     # Encola al jugador en la cola de la partida si es que hay espacio en la cola
     def llegada(self, jugador):
+        ti = datetime.now()
         partida = jugador.getPartida()
         player_id = jugador.getId()
-        ti = datetime.now()
+        self.queues[partida].append(player_id)
+    
+        while self.queues[partida][0] != player_id:
+            self.partidas[partida].event_leave_queue.wait()
+
         self.locks[partida].acquire()
+        
         if self.partidas[partida].isFull():
             self.partidas[partida].event_leave_queue.wait()
             self.partidas[partida].event_leave_queue.clear()
+            
         tf = datetime.now()
         reg_lobby(player_id, str(ti), partida, str(tf))
+        self.queues[partida].pop(0)
         self.locks[partida].release()
         self.partidas[partida].enqueue(jugador)
